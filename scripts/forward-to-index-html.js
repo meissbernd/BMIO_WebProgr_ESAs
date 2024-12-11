ace9143fe33f19f087e541138a5d2930c8da5472
Bernd Meissner <bernd.meissner@gmx.de>
2024-12-11 10:27:31 +0100

    separate forwarding script

---
+++ scripts/forward-to-index-html.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+
+    console.clear();
+    console.log("Start forward-to-index-html");
+
+    setTimeout(() => {
+        window.location.href = 'index.html';
+    }, 3000);
+
+})();
