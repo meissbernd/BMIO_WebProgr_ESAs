8dc5fba3dd94c421ab9112dbd66bc1575330872d
Bernd Meissner <bernd.meissner@gmx.de>
2024-12-11 09:23:57 +0100

    logic to submit when validated

--- scripts/validate-friendly-game.js
+++ scripts/validate-friendly-game.js
@@ -1,8 +1,25 @@
-(function init() {
+(function () {
     "use strict";
 
-    console.clear()
+    console.clear();
     console.log("Start validate-friendly-game:");
 
+    // Todo: button innerhalb des Formulars raussuchen!!
+    const submitButton = document.getElementById("submit");
+    const myForm = document.getElementById('contactForm');
 
+    submitButton.addEventListener("click", (event) => {
+        console.log("Submit clicked");
+        const formDataIsValid = true;
+        if (formDataIsValid) {
+            console.log("Validierung erfolgreich");
+            console.log(myForm.checkValidity());
+            if (myForm.checkValidity()) {
+                myForm.submit();
+            }
+        } else {
+            console.log("Validierung fehlgeschlagen");
+            event.preventDefault();
+        }
+    });
 })();
